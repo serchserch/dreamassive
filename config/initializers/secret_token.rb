b142b2fad696adc42fbf53659984a85b71286aa2
serch <sergio.morlan@globalcorporation.cc>
2014-03-28 11:11:09 -0600

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dreamassive::Application.config.secret_key_base = 'bbee9e01187655a21557c9ff92e46b8b7a25bfce9c5ab03c25704a8dc4ff689b2f90288295712f4ef78614c437de6e2bf318ed5fc46745a1a5e49fb5e527875d'
